fe3bf83cf727b6bc4e29beda3ea2bbc77c42e275
nguyenlyluat-star <nguyenlyluat@gmail.com>
2025-09-14 13:47:41 +0700

    Hoàn thành bài 2

---
+++ Bai2.java
@@ -0,0 +1,15 @@
+public class Bai2 {
+    public static void main(String[] args) {
+        String hoTen = "Nguyen Van A";
+        int tuoi = 20;
+        double chieuCao = 1.75; // mét
+        boolean bietLapTrinh = true;
+        char gioiTinh = 'M';
+
+        System.out.println("Họ tên: " + hoTen);
+        System.out.println("Tuổi: " + tuoi);
+        System.out.println("Chiều cao: " + chieuCao + " m");
+        System.out.println("Biết lập trình: " + bietLapTrinh);
+        System.out.println("Giới tính: " + gioiTinh);
+    }
+}
